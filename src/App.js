2198884842d3665a084fc2444cd36de715ae97eb
VASHKOR-DAS <103654975+VASHKOR-DAS@users.noreply.github.com>
2023-01-22 20:32:47 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUsers = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email }; // user make korlam
+    // console.log(user);
+
+    // server a ai object ta sent krbo
+    fetch('http://localhost:5000/users', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user), // ai user object jeta create korlam seta pathabo, amra data ta body er moddhe pathaici tai server a body te pawar try korbo
+    })
+      .then(res => res.json())
+      .then(data => console.log(data)) // backend theke user k console a pathiye dise
+      .catch(error => console.error(error))
+
+
+
+
+
+
+    event.target.reset();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleAddUsers}>
+        <input type="text" placeholder='Name' required name='name' />
+        <br />
+        <input type="email" placeholder='Email' required name='email' />
+        <br />
+        <button>Add User</button>
+      </form>
+
+      <h2>Users: {users.length}</h2>
+
+      <div>
+        {users.map(user =>
+          <p key={user.id}>
+            {user.name} {user.email}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
